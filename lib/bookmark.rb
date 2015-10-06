3f99430431d3e2900897de7882dc6709d61f5de7
Kablys <mfrohd@gmail.com>
2015-10-06 16:27:32 +0300

    Fix rspec problems

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,15 +1,18 @@
 # Class for handling Bookmarks
 class Bookmark
   @num_of_bookmarks = 0 # should interact with Stat
-  def initialize(link, name)
+  def initialize(link, name = '', tags = [])
+    # TODO: link validation
     @link = link
+    # TODO: if no name is supplied use website title
     @name = name
     @time = Time.now # same as Time.new
+    @tags = tags
   end
 
-  # REVIEW: class instance variable
+  # REVIEW: is this proper class instance variable implementation
 
-  def self.new(link, name)
+  def self.new(link, name = '', tags = [])
     @num_of_bookmarks += 1
     super
   end
@@ -23,8 +26,28 @@ class Bookmark
   #  end
 
   attr_accessor :name, :link
-  attr_reader :time
+  attr_reader :time, :tags
+
+  def add_tag(tag)
+    @tags << tag if tag.is_a? String
+  end
+
+  def del_tag(name)
+    @tags.delete(name)
+  end
+
+  def rename_tag(name, new_name)
+    if (name.is_a? String) && (name.is_a? String)
+      index = @tags.index(name)
+      @tags[index] = new_name
+    else
+      return
+    end
+  end
+
   def to_s
-    "#{@name} -> #{@link} at: #{@time.inspect}"
+    "#{@name} -> #{@link}"
+    # TODO: testing time
+    # "#{@name} -> #{@link} at: #{@time.inspect}"
   end
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,22 +1,86 @@
 require 'spec_helper'
 
 describe Bookmark do
-  before :each do
-    @bookmark = Bookmark.new 'www.example.com', 'Website title'
+  before :all do
+    @bookmark = Bookmark.new('www.example.com',
+                             'Website title',
+                             %w(first example))
   end
+
   describe '#new' do
-    it 'takes 2 parameters and returns a Bookmark object' do
+    it 'takes 3 parameters and returns a Bookmark object' do
       expect(@bookmark).to be_an_instance_of Bookmark
     end
   end
+
   describe '#name' do
     it 'returns the correct name' do
       expect(@bookmark.name).to eql 'Website title'
     end
   end
+
   describe '#link' do
     it 'returns the correct link' do
       expect(@bookmark.link).to eql 'www.example.com'
+    end
+  end
+
+  describe '#tags' do
+    it 'returns array of tags' do
+      expect(@bookmark.tags).to eql %w(first example)
+    end
+  end
+
+  describe '#add_tag' do
+    context 'something other thant string supplied as a tag' do
+      it 'does nothing' do
+        @bookmark.add_tag 4
+        expect(@bookmark.tags).to eql %w(first example)
+      end
+    end
+    context 'one string is suplied as a tag' do
+      it 'adds tag to tag array' do
+        @bookmark.add_tag 'third'
+        expect(@bookmark.tags).to eql %w(first example third)
+      end
+    end
+  end
+
+  describe '#del_tag' do
+    context 'name of to tag to be deleted given' do
+      it 'removes that tag' do
+        @bookmark.del_tag 'third'
+        expect(@bookmark.tags).to eql %w(first example)
+      end
+    end
+  end
+
+  describe '#rename_tag' do
+    context 'gets two strings with names' do
+      it 'renames tag (of first string name) with second string' do
+        @bookmark.rename_tag 'first', 'uno'
+        expect(@bookmark.tags).to eql %w(uno example)
+      end
+    end
+  end
+
+  describe '.num_of_bookmarks' do
+    context 'there being only one bookmark object' do
+      it 'returns 1' do
+        expect(Bookmark.num_of_bookmarks).to eql 1
+      end
+    end
+    context 'there being two bookmark object' do
+      it 'returns 2' do
+        second_bookmark = Bookmark.new 'www.ruby-lang.org/', 'Ruby Language'
+        expect(Bookmark.num_of_bookmarks).to eql 2
+      end
+    end
+  end
+
+  describe '#to_s' do
+    it 'returns correctly formatted string' do
+      expect(@bookmark.to_s).to eql 'Website title -> www.example.com'
     end
   end
 end
