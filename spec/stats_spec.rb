dd2136f410f8ddc9bd8167076f378896e9ec0033
Kablys <mfrohd@gmail.com>
2015-10-25 17:58:57 +0200

    stat specs moved to stats_spec

---
+++ spec/stats_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Stats do
+  describe '#Bookmark' do
+    context 'get total number of bookmarks' do
+    end
+    context 'get total number of tags' do
+    end
+    context 'get total number of bookmarks with same tag' do
+    end
+    context 'get total number of bookmarks from same domain' do
+    end
+  end
+end
